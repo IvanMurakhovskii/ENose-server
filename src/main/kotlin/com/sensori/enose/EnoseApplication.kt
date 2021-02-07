9f4c923df8b8084ceab5e61d82df16965d5c3795
IvamMurakhovskii <ivan112247@gmail.ru>
2021-02-07 17:21:34 +0300

    init spring boot app

---
+++ src/main/kotlin/com/sensori/enose/EnoseApplication.kt
@@ -0,0 +1,11 @@
+package com.sensori.enose
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EnoseApplication
+
+fun main(args: Array<String>) {
+    runApplication<EnoseApplication>(*args)
+}
